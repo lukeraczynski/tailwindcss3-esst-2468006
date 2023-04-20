9755457025e3f56be1bb77234cfac36c72dde9d5
Luke Raczynski <55918691+lukeraczynski@users.noreply.github.com>
2023-04-20 19:55:05 +0000

    add class csv opener

--- tester.py
+++ tester.py
@@ -1,1 +1,12 @@
 # tester file for the project
+
+class OpenCSV:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        self.file = open(self.filename, 'r')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()

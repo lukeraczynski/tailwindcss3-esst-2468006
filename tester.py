d6d0797dacdd8047a20a2c7937a8a564d1ad6d74
Luke Raczynski <55918691+lukeraczynski@users.noreply.github.com>
2023-05-24 19:59:28 +0000

    add a line

--- tester.py
+++ tester.py
@@ -22,3 +22,14 @@ class OpenCSV:
         with open(self.filename, 'r') as f:
             print(f.read()) 
             
+
+
+class Name(models.Model):
+    first_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+        
